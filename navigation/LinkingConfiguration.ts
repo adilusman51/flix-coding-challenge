5fd370981c1a2b66c86cc7d377c404c821ebf04c
Adil Usman <adilusman51@gmail.com>
2022-08-16 19:02:52 +0500

    feat: Format with Prettier

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -10,27 +10,22 @@ import * as Linking from 'expo-linking';
 import { RootStackParamList } from '../types';
 
 const linking: LinkingOptions<RootStackParamList> = {
-  prefixes: [Linking.createURL('/')],
-  config: {
-    screens: {
-      Root: {
-        screens: {
-          TabOne: {
-            screens: {
-              TabOneScreen: 'one',
-            },
-          },
-          TabTwo: {
-            screens: {
-              TabTwoScreen: 'two',
-            },
-          },
-        },
-      },
-      Modal: 'modal',
-      NotFound: '*',
-    },
-  },
+	prefixes: [Linking.createURL('/')],
+	config: {
+		screens: {
+			Root: {
+				screens: {
+					Users: {
+						screens: {
+							UsersScreen: 'user',
+						},
+					},
+				},
+			},
+			Modal: 'modal',
+			NotFound: '*',
+		},
+	},
 };
 
 export default linking;
