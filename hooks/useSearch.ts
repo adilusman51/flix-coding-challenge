6e01ca5211f2060e3ff03207318606994e154bab
Adil Usman <adilusman51@gmail.com>
2022-08-18 00:12:24 +0500

    feat: Search with debounce

---
+++ hooks/useSearch.ts
@@ -0,0 +1,140 @@
+import { useCallback, useEffect, useReducer, useRef } from 'react';
+
+const DEFAULT_TIMEOUT = 1000 * 60 * 60 * 1;
+
+interface State<T> {
+	loading: boolean;
+	data?: T;
+	error?: Error;
+	timestamp?: number;
+}
+
+type Action<T> =
+	| { type: 'loading' }
+	| { type: 'success'; payload: T; timestamp: number }
+	| { type: 'cache'; payload: T; timestamp: number }
+	| { type: 'error'; payload: Error };
+
+type SearchOptions = {};
+
+function useSearch<T = unknown>(
+	searchQuery: string,
+	method: Promise<T>,
+	options?: SearchOptions
+): State<T> & { refetch: () => Promise<void> } {
+	// Used to prevent state update if the component is unmounted
+	const cancelRequest = useRef<boolean>(false);
+
+	const initialState: State<T> = {
+		error: undefined,
+		data: undefined,
+		loading: false,
+	};
+
+	// Keep state logic separated
+	const fetchReducer = (state: State<T>, action: Action<T>): State<T> => {
+		switch (action.type) {
+			case 'loading':
+				return { ...initialState, loading: true };
+			case 'success':
+				return {
+					...initialState,
+					data: action.payload,
+					loading: false,
+					timestamp: action.timestamp,
+				};
+			case 'cache':
+				return {
+					...initialState,
+					data: action.payload,
+					loading: false,
+					timestamp: action.timestamp,
+				};
+			case 'error':
+				return {
+					...initialState,
+					error: action.payload,
+					loading: false,
+				};
+			default:
+				return state;
+		}
+	};
+
+	const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+	const filter = (data: T, searchQuery: string) => {
+		let filteredData = [];
+		if (data instanceof Array) {
+			filteredData =
+				data?.filter((item, index) => {
+					let found = false;
+					if (item instanceof Object) {
+						const values = Object.values(item);
+						values.forEach((element) => {
+							if (typeof element === 'string') {
+								if (
+									element
+										.toLowerCase()
+										.includes(searchQuery.toLowerCase())
+								) {
+									found = true;
+								}
+							} else if (typeof element === 'number') {
+								if (element.toString()?.includes(searchQuery)) {
+									found = true;
+								}
+							}
+						});
+					}
+					if (found) {
+						return true;
+					} else {
+						return false;
+					}
+				}) || [];
+		}
+		return filteredData;
+	};
+
+	const refetch = useCallback(async () => {
+		try {
+			const data = await method;
+
+			const timestamp = Date.now();
+			const filteredData = filter(data, searchQuery);
+			console.log(`[success]:`, filteredData, timestamp);
+			if (cancelRequest.current) return;
+			dispatch({
+				type: 'success',
+				payload: filteredData as unknown as T,
+				timestamp: timestamp,
+			});
+		} catch (error) {
+			console.log(`[error]:`, error);
+			if (cancelRequest.current) return;
+			dispatch({ type: 'error', payload: error as Error });
+		}
+	}, [searchQuery]);
+
+	useEffect(() => {
+		cancelRequest.current = false;
+
+		const searchData = async () => {
+			refetch();
+		};
+
+		void searchData();
+
+		// Use the cleanup function for avoiding a possibly...
+		// ...state update after the component was unmounted
+		return () => {
+			cancelRequest.current = true;
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [searchQuery]);
+
+	return { ...state, refetch };
+}
+
+export default useSearch;
--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,23 +1,31 @@
 import { MaterialIcons } from '@expo/vector-icons';
-import { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { KeyboardAvoidingView, ScrollView, StyleSheet } from 'react-native';
+import Searchbar from '../components/SearchBar';
 
 import TableList from '../components/TableList';
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
+import useDebounce from '../hooks/useDebounce';
 import useFetch from '../hooks/useFetch';
+import useSearch from '../hooks/useSearch';
 import { ApiClient, User } from '../services/ApiClient';
 import { AppStackScreenProps } from '../types';
 
 export default function UsersScreen({
 	navigation,
 }: AppStackScreenProps<'Users'>) {
-	const { data, loading, error, refetch } = useFetch<User[]>(
-		ApiClient.fetchUsers(),
-		{
-			cacheKey: 'Users',
-			cacheTimeOut: 1000 * 60 * 60 * 1,
-		}
-	);
+	const [searchString, setSearchString] = useState<string>('');
+	const searchQuery = useDebounce(searchString, 500);
+	const {
+		data: listData,
+		loading: loadingList,
+		error: errorList,
+		refetch,
+	} = useFetch<User[]>(ApiClient.fetchUsers(), {
+		cacheKey: 'Users',
+		cacheTimeOut: 1000 * 60 * 60 * 1,
+	});
+	const { data: searchData } = useSearch(searchQuery, ApiClient.fetchUsers());
 	useEffect(() => {
 		navigation?.setOptions({
 			headerRight: ({ tintColor }) => (
@@ -33,22 +41,41 @@ export default function UsersScreen({
 		return () => {};
 	}, []);
 
+	const onChangeText = (text: string) => {
+		setSearchString(text);
+	};
+	const onClearText = () => {
+		setSearchString('');
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Users</Text>
-			<View
-				style={styles.separator}
-				lightColor='#eee'
-				darkColor='rgba(255,255,255,0.1)'
-			/>
-			<TableList
-				headers={[
-					{ label: 'name', key: 'name' },
-					{ label: 'age', key: 'age' },
-				]}
-				data={data}
-			/>
-		</View>
+		<KeyboardAvoidingView style={{ flex: 1 }}>
+			<ScrollView
+				contentContainerStyle={{ flex: 1 }}
+				keyboardDismissMode='interactive'
+			>
+				<View style={styles.container}>
+					<Searchbar
+						placeholder='Search Users'
+						value={searchString}
+						onChangeText={onChangeText}
+						onClearText={onClearText}
+					/>
+					<View
+						style={styles.separator}
+						lightColor='#eee'
+						darkColor='rgba(255,255,255,0.1)'
+					/>
+					<TableList
+						headers={[
+							{ label: 'Name', key: 'name' },
+							{ label: 'Age', key: 'age' },
+						]}
+						data={searchQuery ? searchData : listData}
+					/>
+				</View>
+			</ScrollView>
+		</KeyboardAvoidingView>
 	);
 }
 
